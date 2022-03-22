934e6f7ae11f1804d9e7509c00cb021faa13fb51
Garrett <partenza.g@northeastern.edu>
2022-03-21 21:16:05 -0400

    done v1

--- slam.py
+++ slam.py
@@ -61,18 +61,19 @@ class Triangle():
             return point3D, 1
         else:
             return point3D, 0
+        
 
         
 # Run slam on a video path
 def slam(video):
     
-#     import numpy as np
-#     import matplotlib.pyplot as plt
-#     from mpl_toolkits.mplot3d import Axes3D
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
 
-#     plt.ion()
-#     fig = plt.figure(figsize=(15,10))
-#     ax = fig.add_subplot(111, projection='3d')
+    plt.ion()
+    fig = plt.figure(figsize=(15,10))
+    ax = fig.add_subplot(111, projection='3d')
     
 #     location = np.array([0,0,0])
     
@@ -88,9 +89,9 @@ def slam(video):
             
             # Feature map current frame to last frame 
             matches, kps1, des1 = extractor.extract(frame)
-                        
-            if matches:
-                print(dir(matches[0]))
+
+            if matches:               
+                
                 # Pose transformation
                 matches, R, t = getPose(kps0, kps1, matches)
                 # Triangulation
@@ -98,26 +99,7 @@ def slam(video):
                 for match in matches:
                     world, mask = triangulator.triangulate(K, R, t, kps0[match.queryIdx].pt, kps1[match.trainIdx].pt)
                     if world[:,2] > 0 and mask:
-                        points.append(world)
-#                 triangulator.R = R
-#                 triangulator.t = t
-                
-#                 ax.scatter(location[0], location[1], location[2], s=50, c='lime')
-                
-                #filter far points
-#                 points = [point for point in points if np.sqrt(point[0][0]**2+point[0][1]**2+point[0][2]**2) < 200]
-                
-#                 x_data = [point[0][0]+location[0] for point in points]
-#                 y_data = [point[0][1]+location[1] for point in points]
-#                 z_data = [point[0][2]+location[2] for point in points]
-
-#                 ax.scatter(x_data, y_data, z_data, s=0.5, c='blue', alpha=0.2)
-#                 plt.draw()
-#                 plt.pause(0.1)
-
-#                 location = (np.dot(triangulator.R, location)+np.transpose(triangulator.t)).flatten()
-
-                
+                        points.append(world)                
                     
                 # Plot on frame
                 for match in matches:
@@ -131,14 +113,35 @@ def slam(video):
             if not ret:
                 vid.release()
                 break
+                
 
             # Display frame
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
             cv2.imshow('Frame', frame)
+            
+            if matches:
+                # specify the location of (left,bottom),width,height
+                import matplotlib.patches as mpatches
+                rect=mpatches.Rectangle((0,0),14,7, 
+                                        fill = False,
+                                        color = "purple",
+                                        linewidth = 2)
+                points = [point for point in points if np.sqrt(point[0][0]**2+point[0][1]**2+point[0][2]**2) < 400]
+                x_points = [point[0][0] for point in points]
+                y_points = [point[0][1] for point in points]
+                z_points = [point[0][2] for point in points]
+                ax.scatter(x_points, y_points, z_points, s=3, c='blue', alpha=0.5)
+                ax.scatter([0], [0], [0], s=50, c='green', alpha=1.0)
+                plt.draw()
+                plt.pause(5)
+                
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
             elif cv2.waitKey(25) & 0xFF == ord('p'):
                 cv2.waitKey(-1)
+            
+            ax.cla()
 
     except KeyboardInterrupt:
         vid.release()
@@ -158,3 +161,33 @@ if __name__ == "__main__":
     W = 1920
     K = np.array([[F, 0, W//2], [0, F, H//2], [0, 0, 1]], dtype=np.float32)
     slam(video)
+    
+                    # Convert to normalized homogenous 
+#                 pts1 = np.int32([kps0[match.queryIdx].pt for match in matches]) 
+#                 pts2 = np.int32([kps1[match.trainIdx].pt for match in matches])
+#                 points1 = [np.hstack([point, [1]]) for point in pts1]
+#                 points2 = [np.hstack([point, [1]]) for point in pts2]
+#                 points1_norm = np.array([np.dot(np.linalg.inv(K), point) for point in points1])
+#                 points2_norm = np.array([np.dot(np.linalg.inv(K), point) for point in points2])
+#                 # Compute pose
+#                 p1n, T1 = scale_and_transform_points(points1_norm)
+#                 p2n, T2 = scale_and_transform_points(points2_norm)
+#                 # Compute F
+#                 F = compute_img_to_img_matrix(p1n, p2n, False)
+#                 F = np.dot(T1.T, np.dot(F, T2))
+#                 E = F / F[2, 2]
+#                 P1 = np.array([[1,0,0,0], [0,1,0,0], [0,0,1,0]])
+#                 P2s = compute_P_from_essential(E)
+#                 ind = -1
+#                 for i, P2 in enumerate(P2s):
+#                     d1 = reconstruct_one_point(points1_norm[:, 0], points2_norm[:, 0], P1, P2)
+#                     P2_homogenous = np.linalg.inv(np.vstack([P2, [0,0,0,1]]))
+#                     d2 = np.dot(P2_homogenous[:3, :4], d1)
+#                     if d1[2] > 0 and d2[2] > 0:
+#                         ind = i
+#                 P2 = np.linalg.inv(np.vstack([P2s[ind], [0, 0, 0, 1]]))[:3, :4]
+#                 # Triangulate
+#                 tripoints3d = triangulation(points1_norm, points2_norm, P1, P2)
+#                 x_points = [pt for pt in tripoints3d[0]]
+#                 y_points = [-pt for pt in tripoints3d[1]]
+#                 z_points = [-pt for pt in tripoints3d[2]]
